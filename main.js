b7d7fefbcfe14bf034f5e7bb11198e432ecd3cad
Matty-Coding <mattyloffry.01@gmail.com>
2026-04-27 21:20:32 +0200

    template completed, mobile first responsive + hamburger menu

---
+++ main.js
@@ -0,0 +1,10 @@
+// toggle sidebar
+const sideBar = document.querySelector("aside");
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+
+function toggleSideBar() {
+  sideBar.classList.toggle("-translate-x-[105%]");
+  sideBar.classList.toggle("translate-x-0");
+}
+
+//
